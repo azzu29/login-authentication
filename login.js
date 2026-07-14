96caeaf26bd8dc708c3d4889521d99367c5cffac
Anas Aziz <115024609+azzu29@users.noreply.github.com>
2026-07-15 00:35:19 +0530

    Implement user authentication on login form
    Refactor login form submission to validate user credentials against users.json and display error message for invalid login.

--- login.js
+++ login.js
@@ -1,7 +1,32 @@
-document
-  .getElementById("loginForm")
-  .addEventListener("submit", function (e) {
+// document
+//  .getElementById("loginForm")
+//  .addEventListener("submit", function (e) {
+//    e.preventDefault();
+
+//    window.location.href = "ceo_profile_card.html";
+//  });
+document.getElementById("loginForm").addEventListener("submit", function(e) {
     e.preventDefault();
 
-    window.location.href = "ceo_profile_card.html";
-  });
+    const email = document.getElementById("email").value;
+    const password = document.getElementById("password").value;
+
+    fetch("users.json")
+        .then(response => response.json())
+        .then(users => {
+
+            const user = users.find(u =>
+                u.email === email &&
+                u.password === password
+            );
+
+            if (user) {
+                window.location.href = "ceo_profile_card.html";
+            } else {
+                document.getElementById("error").innerText =
+                    "Invalid Email or Password";
+            }
+
+        });
+});
+
